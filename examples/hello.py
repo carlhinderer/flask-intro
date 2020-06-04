8294fe4b81527573d4ada857902b8b6b4904c151
Carl Hinderer <carl.hinderer4@gmail.com>
2020-06-04 17:17:32 -0600

    Added simple name form.

--- examples/hello.py
+++ examples/hello.py
@@ -9,6 +9,15 @@ bootstrap = Bootstrap(app)
 moment = Moment(app)
 
 
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+
 @app.route('/')
 def index():
     return render_template('index.html', current_time=datetime.utcnow())
